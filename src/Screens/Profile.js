a370b5707b77c4cb72d621b8228f6446bbe43150
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-04-03 22:13:46 -0400

    profile page has been added

---
+++ src/Screens/Profile.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react';
+
+export default class Profile extends Component {
+    render() {
+        return <div>
+            <p>Hello, this is a profile page</p>
+        </div>
+    }
+}
