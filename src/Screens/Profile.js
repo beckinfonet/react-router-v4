7711af83d35c490addb6bcea5f83c72fe4b86928
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-04-05 19:34:28 -0400

    map component has some issues related to the updates made after its creation

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,9 +1,12 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
 
 export default class Profile extends Component {
-    render() {
-        return <div>
-            <p>Hello, this is a profile page</p>
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <p>Hello, this is a profile page</p>
+      </div>
+    );
+  }
 }
+// this is not on master
